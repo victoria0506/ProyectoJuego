e975d55a901d3d04180e5381abc9d2f8db23f3f0
victoria0506 <vicguti527@gmail.com>
2024-05-18 15:29:14 -0600

    fun

--- java/java.js
+++ java/java.js
@@ -1,35 +1,47 @@
 
 
-let jugadorUno = true
+
+
 let juego = document.getElementsByClassName("celda")
 
+let jugadorUno = true
 //console.log(juego);
 
-const matriz = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
+const Ganador = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
 ];
-
-
+//console.log(Ganador);
 
 for (let index = 0; index < juego.length; index++) {
+
     juego[index].addEventListener("click", function(e){
-        console.log("diste click");
-        juego.innerText = "✘"
 
+     //console.log("diste click");
 
+    if (jugadorUno){
 
+        e.target.innerHTML = "✘" 
+        jugadorUno = false
+
+    }else{
+
+        e.target.innerHTML = "◯" 
+        jugadorUno = true
+    }
+
+    juego.disabled = true
 
     })
 }
 
 
-/*
-let imprimir = e.target.innerHTML = jugadorUno? "✘" : "◯"
-    
-jugadorUno = !jugadorUno
-*/
 
 
 
